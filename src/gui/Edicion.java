66cbc0d58c52a8fe440c41fe2eecc83721cd6101
wjoelmendoza <wjoelmendoza@gmail.com>
2015-08-06 01:10:54 -0600

    Actualizacion Se agrego soporte ala matriz para la eliminacion a traves del numero de columna se corrigio un error que se producia al momento de eliminar al heroe o al castillo desde la ventana de edicion se agrego soporte a la lista, para que se pueda trabajar en modo pila o cola se agrego sopoete a la lista, para que permitiera la visualizacion del elemto siguiente segun el comportamiento elegido

--- src/gui/Edicion.java
+++ src/gui/Edicion.java
@@ -6,9 +6,7 @@
 package gui;
 
 import estructura.ListaDoble;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JLabel;
+import java.awt.event.WindowEvent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -19,18 +17,20 @@ import javax.swing.JScrollPane;
 public class Edicion extends javax.swing.JFrame {
     private final ListaDoble ld;
     private JPanel jp;
+    private CargaObjetos pr;
     /**
      * Creates new form Edicion
      * @param ld
+     * @param principal
      */
-    public Edicion(ListaDoble ld) {
+    public Edicion(ListaDoble ld, CargaObjetos principal) {
         super("Edicion de Objetos");
         initComponents();
         this.setResizable(false);
         jp = new JPanel();
         this.ld = ld;
         ld.mostrarObj(jp);
-        
+        pr =principal;
         JScrollPane jsp = new JScrollPane(jp);
         jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -50,23 +50,61 @@ public class Edicion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Aceptar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 555, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(234, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(234, 234, 234))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 523, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(486, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(ld.getCastillo()==0)
+            pr.habilitarCastillo(true);
+        
+        if(ld.getHeroe() == 0)
+            pr.habilitarMario(true);
+        
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
+    @Override
+    protected void processWindowEvent(final WindowEvent e){
+        
+        if(ld.getCastillo()==0)
+            pr.habilitarCastillo(true);
+        
+        if(ld.getHeroe() == 0)
+            pr.habilitarMario(true);
+        
+        super.processWindowEvent(e);
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
